ef9a989c6842e11c3986f6a969ee4adeebf29511
Gabe-Matherley <157724600+Gabe-Matherley@users.noreply.github.com>
2024-02-10 21:36:41 -0500

    Update 3
    mess with code

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    if word[0:2] == word[0:-2]:
+    if word[0:2] == word[-2:][::-1]:
         return True
     else:
         return False
 
 if __name__ == '__main__': 
-    #REMOVE PASS AND YOUR CODE GOES HERE
-
+    #REMOVE PASS AND YOUR CODE GOES HERE  
     print(palindrome(word))
